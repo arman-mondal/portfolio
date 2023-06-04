c8536162171dd3ef21cd2f36d5e5d458c56bb1b4
arman-mondal <admin@armanmondal.in>
2023-06-04 12:23:09 +0530

    new file:   .env 	new file:   .gitignore 	new file:   LICENSE 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/dev.jpeg 	new file:   public/index.html 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/app/App.css 	new file:   src/app/App.js 	new file:   src/app/routes.js 	new file:   src/assets/images/dev.jpeg 	new file:   src/assets/images/logo.svg 	new file:   src/assets/images/react portfolio gif.gif 	new file:   src/assets/images/react-porfolio.png 	new file:   src/components/socialicons/index.js 	new file:   src/components/socialicons/style.css     new file:   src/components/themetoggle/index.js     new file:   src/components/themetoggle/style.css 	new file:   src/content_option.js 	new file:   src/header/index.js 	new file:   src/header/style.css 	new file:   src/hooks/AnimatedCursor.js 	new file:   src/hooks/withRouter.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/pages/about/index.js 	new file:   src/pages/about/style.css 	new file:   src/pages/contact/index.js 	new file:   src/pages/contact/style.css 	new file:   src/pages/home/index.js 	new file:   src/pages/home/style.css 	new file:   src/pages/portfolio/index.js 	new file:   src/pages/portfolio/style.css 	new file:   src/reportWebVitals.js 	new file:   yarn.lock

---
+++ src/content_option.js
@@ -0,0 +1,131 @@
+const logotext = "Soumyajit Dey";
+const meta = {
+    title: "Soumyajit Dey",
+    description: "I’m Soumyajit Dey data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Soumyajit Dey",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I am a student of Electronics and Communication Engineering and the domains that I am specialized include electronic circuit and coding.",
+    your_img_url: "dev.jpeg",
+};
+
+const dataabout = {
+    title: "About My Self",
+    aboutme: " I am a student of Electronics and Communication Engineering and the domains that I am specialized include electronic circuit and coding.",
+};
+const worktimeline = [{
+        jobtitle: "B.P. Poddar Institute of Management and Technology",
+        where: "B.Tech in Electronics and Communication engineering",
+        date: "9.60 SGPA",
+    },
+    {
+        jobtitle: "Central Modern School, Kolkata",
+        where: "I.S.C.",
+        date: "88%",
+    },
+    {
+        jobtitle: "Central Modern School, Kolkata",
+        where: "I.C.S.E ",
+        date: "87%",
+    },
+];
+
+const skills = [{
+        name: "C",
+        value: 100,
+    },
+    {
+        name: "JAVA",
+        value: 100,
+    },
+    {
+        name: "C++",
+        value: 100,
+    },
+    {
+        name: "Firebase",
+        value: 75,
+    },
+    {
+        name: " Tailwind CSS",
+        value: 80,
+    },
+
+    {
+        name: " Bootstrap4",
+        value: 83,
+    },
+    {
+        name: "Nest js",
+        value: 86,
+    },
+    {
+        name: " JavaScript",
+        value: 88,
+    },
+    {
+        name: "MySQL",
+        value: 79,
+    },
+    {
+        name: "PHP",
+        value: 81,
+    },
+    {
+      name:" React JS, React native, MongoDB .node.js ,Material UI , Vue JS, React native , express.js ",
+      value:100,
+    },
+
+
+];
+
+const services = [{
+        title: "",
+        description: "I wish to pursue my career at a company that allows me to make the best use  of my technical and interpersonal skills. I am willing to explore a wide range of opportunities that  allow me to gain more knowledge and perspective .  To work in an organization where I can use my skills and knowledge to deliver value-added results   that provide me job satisfaction and self-development which help me achieve personal as well as organizational goals.",
+    },
+  
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+  
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "iamsoumyajitdey@gmail.com",
+    YOUR_FONE: "(+91) 7044203915",
+    description: "I am a student of Electronics and Communication Engineering and the domains that I am specialized include electronic circuit and  coding.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_zrkkmnt",
+    YOUR_TEMPLATE_ID: "template_pnsz4un",
+    YOUR_USER_ID: "user_U9DIZMOq49ssSmBLEGCWj",
+};
+
+const socialprofils = {
+    github: "https://github.com/soumyajitdey",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
